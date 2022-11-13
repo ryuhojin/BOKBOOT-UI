fc403b83eff96956a610977bd771955626d111c7
ryuhojin <interarea@naver.com>
2022-11-13 16:05:28 +0900

    업데이트 : 리액트 GA 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
-
+import ReactGA from 'react-ga4'
 import Root from "@/Root";
 import "@/styles/index.css";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./libs/api/mocks/browser");
   worker.start();
+  ReactGA.initialize("G-G3HR6C70SS");
 }
 
 const container = document.getElementById("root");
