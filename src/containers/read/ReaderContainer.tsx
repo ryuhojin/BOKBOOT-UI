7b665e43c0e9e7d29539ec6e51f4b1fe386f044e
류호진 <interarea@naver.com>
2022-11-30 20:41:20 +0900

    업데이트 : 멀티리더 기능 로직작성

--- src/containers/read/ReaderContainer.tsx
+++ src/containers/read/ReaderContainer.tsx
@@ -1,4 +1,31 @@
+import { useCallback, useEffect, useState } from "react";
+import { useContent, useContents, useMessage, useTheme } from "@/libs/hooks";
+import clipboard from "@/libs/utils/clipboard";
+import service from "@/libs/api";
+import { useSearchParams } from "react-router-dom";
+
 const ReaderContainer = () => {
+  const [contents, , onChange] = useContent("");
+  const [, onMessages] = useMessage();
+  const [, , themeMode] = useTheme(); //테마(다크,라이트)
+  const [language, setLanguage] = useState("");
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (!searchParams.get("id") && !searchParams.get("key")) return;
+    service
+      .get(`/sourceCode/${searchParams.get("id")}`, {
+        key: searchParams.get("key"),
+      })
+      .then((res) => {
+        console.log(res)
+      });
+  }, [searchParams]);
+
+  const onClickCopy = useCallback((code: string) => {
+    onMessages(clipboard(code, "Copy Code Sucessful"));
+  }, []);
+  
   //TODO : API호출후 여러개인지 판별하여 소스 뿌리기
   return <></>;
 };
--- src/libs/hooks/index.ts
+++ src/libs/hooks/index.ts
@@ -4,3 +4,4 @@ export { default as useMessage } from "./useMessage"; //Toast Hook
 export { default as useTheme } from "./useTheme"; // Theme Hook
 export { default as usePopup } from "./usePopup"; //Popup Hook
 export { default as useMulti } from "./useMulti"; //Multi Hook
+export { default as useContents } from './useContents';//Editor Inputs Hook
